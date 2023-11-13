dcbab318f0a7f9f5dc73815cfae4aa9203bc78c0
sruthiek <sruthi.ek2580@gmail.com>
2023-11-13 19:00:22 +0530

    demo-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+   this.example();
+  }
   title = 'demo-2';
+
+  example() {
+    var variable1 = 'hello';
+    let variable2 = 'hello user';
+    const variable3 = 'welcome';
+
+    if (2 > 1) {
+      var variable1 = 'good catch';
+      let variable2 = ' hello bye';
+      console.log('first', variable1);
+      console.log('second', variable2);
+      console.log('third', variable3);
+    }
+    console.log('forth', variable1);
+    console.log('fifth', variable2);
+    console.log('sixth', variable3);
+  }
+
+
 }
+
